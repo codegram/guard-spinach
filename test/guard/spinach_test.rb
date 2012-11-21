94f540fd8d22df7343990b476ef4eed47901ab33
Brendon Murphy <xternal1+github@gmail.com>
2012-11-20 16:43:24 -0800

    Fix a broken spec and load mocha after minitest
    Mocha depends on having the test framework loaded first now to
    know how to reset some things after runs, etc.

--- test/guard/spinach_test.rb
+++ test/guard/spinach_test.rb
@@ -2,15 +2,16 @@ require_relative '../test_helper'
 
 describe Guard::Spinach do
   subject do
-    Guard::Spinach.new(data)
+    Guard::Spinach.new
   end
   let(:paths) do
     ['fake/path.feature', 'foo/bar.feature']
   end
-  describe "#run_on_change" do
+  describe "#run_on_changes" do
     it "fires run on a runner" do
       Guard::Spinach::Runner.any_instance.expects(:system).with(
         "spinach fake/path.feature foo/bar.feature")
+      subject.run_on_changes(paths)
     end
   end
 end
